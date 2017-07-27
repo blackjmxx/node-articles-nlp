4c98ca05e554d1715437ed97a09ad04ea9c98c47
blacjmxx2 <alandou.jeanmichel@gmail.com>
2017-07-27 20:06:42 +0200

    d

--- script/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ "$ENABLE_DEBUG" == "true" ]; then
-	echo "Starting with debugger on port $AUX_PORT"
-	exec with_ngrok node --debug=$AUX_PORT lib/worker.js
-else
-	echo "Starting without debugger"
-	exec node lib/worker.js
-fi
